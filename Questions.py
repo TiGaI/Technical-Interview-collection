9f335f9097289719a828eb72fbab4a9b3b9e2e73
Junjie Jiang <junjiejiang94@gmail.com>
2018-01-25 19:24:29 -0500

    firstpush 4 question

---
+++ Questions.py
@@ -0,0 +1,13 @@
+Question = {
+"1": "Given a string representing a phrase e.g. om is in Austin, reverse each word with the location of the world being in the same place e.g. moT si ni nitsuA",
+
+
+"2": "What other companies are you applying to, and why are you applying to them?"	,
+
+"3": 
+
+"4":
+
+}
+
+ 
