f5a782864c4d4c7a233be79404222d46a84f33ad
Junjie Jiang <junjiejiang94@gmail.com>
2018-03-05 03:26:27 -0500

    addstockQuant

---
+++ stockingPythonQuestion.py
@@ -0,0 +1,80 @@
+What is stochastic calculus in finance use for: 
+modeling the random option of an asset price in the black-scholes modeling
+Used to model the random motion of an asset price and allow derivative to have a random component determined by a Brownian motion
+
+what is Black Scholes model:
+a pricing model use to determine the fair price or theoretical value for a call or a put option based on six variables:
+	1. Volatility
+	2. Type of option
+	3. Underlying stock price
+	4. Time
+	5. Strike Pirce
+	5. risk free rate
+
+
+Stochastic Oscillator - a momentum indicator that use support and resistance levels
+%K = (Price-L5)/(H5-L5) -> L5 is the lowest price in 5 days
+The idea behind this indicator is that prices tend to close near the extremes of the recent range before turning points.
+The four common type of financial Derivative:
+	1. Forwards
+	2. Futures
+	3. Options
+	4. Swap - most common type of derivative - agreement to exchange one asset or debt for a similar one
+
+four risks:
+	1. Nearly impossible to know anyt derivative real value - 
+	2. Leaverage - huge loss if everyone is doing and predicted incorrectly, help cause 2008 housing market
+	3. Time restriction - unregulated and not sold on exchanges
+	4. Potential for scams
+
+Market Microstructure
+
+
+The Bernouilli Process-
+
+
+
+Options Greeks:
+
+Why buy options? 
+	Speculation - mininal commitment and only need to pay a premium. Calls
+	Hedging - reduces risk at a reasonable cost. Puts
+	Spreading - use of two or more options positions
+	Synthetics - Avoid legal or regulatory reason for owning it. Allow one to gain without "actually" owning the stock
+
+Four Cardinal Coordinates: Holder - people who buy option, Writer - peopel who options 
+	1. Buy Calls - has the right to buy at a certain prices - don't mean they will, they just have the rights, but they will loss their premium of their options
+	2. Sell Calls - are obligated to sell - have unlimited risk and can lose much more than the price of the options premium
+	3. Buy Puts - has the right to sell at a certain prices - don't mean they will, they just have the rights, but they will loss their premium of their options
+	4. Sell Puts - are obligated to buy - have unlimited risk and can lose much more than the price of the options premium
+
+Premium is determined by factors including the stock price, strike price, time remaining until expiration and volatility
+for call options: 
+In the money if the share price is above the strike price
+out of the money if the share price is below the strike price
+
+
+What is the Greeks in Options?
+	1. Delta - measures the sensitvity of an option's value to a price change in the underlying asset.
+		Call Option + Put Options -
+	2. Gamma - measures the rate of change in delta for each one-point increase in the underlying asset
+	3. Theta - The dollar amount an option loses with each day that passes
+		Increase near expiration, decrease in and out of the money options
+	4. Vega - meausres an option's sensitvity to changes in Volatility.
+		Increase if volativility increase 
+They all measure the sensentive of price
+
+
+10 Options Strategies to Know
+	1. Cover call - purchase the assets outright and simultaneously write a call option on those same assets
+	2. Married Put - purchase or currently owns a particular assets simultaneously purchases a put option for an equivalent number of shares - insurance policy and establishes a floor should the asset's price plunge dramatically.
+	3. Bull Call Spread - purchase buy call and sell calls at a higher strike price.
+	4. Bear Put Spread - reverse of bull call Spread
+	5. Protective Collar - Purchasing an out of the money put option and writing an out of money call option at the same time - to lock in profits - an advance form for cover call
+	6. Long Straddle - purchase both call and put option with the same strike price, believing that the stock will move significantly but the direction is unknown
+	7. Long Strangle - purchase both call and put option with the different strike prices. Less expensive than straddle due to out of money
+	8. Butterfly Spread - purchase a spread of call or put options
+	9. Iron Condor - holds a long and short position in two different strangle Strategies
+	10. Iron Butterfly - don't understand.
+
+
