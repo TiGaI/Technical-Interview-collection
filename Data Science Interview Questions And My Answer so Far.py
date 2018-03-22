8d0d0908fe870229e290bc59f5cdc9fa47a5d9c0
Junjie Jiang <junjiejiang94@gmail.com>
2018-03-21 21:52:02 -0400

    test

---
+++ Data Science Interview Questions And My Answer so Far.py
@@ -0,0 +1,56 @@
+1. Compare Data Science Vs. Machine Learning
+Data Science - Multidisciplinary, Lossely Integrated, and can take on a business role
+Machine Learning - Training Machines to predict or charactersics, tightly integrated, and purely technical role
+
+2. What is Data Science?
+Data Science is a blend of Statistics, technical skills and business vision which is used to analyze the available data and predict the future trend.
+
+3. How is it different from big data and data analytics?
+big data - huge volumes of data-structured, unstructured and semi-structured and requires a basic knowledge of statistics and mathematics
+data science - deals with slicing and dicing the data, requires in depth knowledge of statistics and mathematics
+data analytics - contributing operational insights into complex business scenarios - require moderate amount of statistics and mathematics
+
+4. Compare SAS, R and Python programming
+SAS - most widely used analytics tools - some of the best statistical functions, graphical user interface, but has a price tag and not easily adopted by smaller enterprises
+R - a Open source tool use generously by academia and research community. It is a robust tool for statistical computation, graphical representation and reporting. Open Source Nature
+Python - Power open source programming language and east to learn, work well. Innumerable libraries and community created modules. Very robust
+
+5. Various benefits of R language?
+A extensive collection of tools for data analysis
+Operators for performing calculations on matrix and array
+Data analysis technique for graphical representation
+Simple and effective language
+Support ML applications
+Database and various of dataset supports
+Robust package ecosystem
+Solve data-oriented problem
+
+Need to learn basic R
+
+6. WHat are the two main components of the Hadoop Framework?
+HDFS - Hadoop Distrubted File System - a distributed database working on top of Hadoop. Storing and retrieving bulk of datasets in no time
+YARN - Yet Another Resource Negotiator - It allocates resources dynamically and handles the workloads.
+
+7. Univariate, bivariate and multivariate analysis
+Univariate - one variable and no relationship causes. _ summarize the data and find patterns within it to make actionable decisions
+Bivariate - two variable - chi-squared tests and t-tests to see if they are a correlations
+Multivariate - three or more - find the correlations between each variables and their potential causes and patterns
+
+8. Power analysis
+Power analysis lets you understand the sample size estimate so that they are neither high or low. Kets you determine the sample size needed for detecting an effect of a given size
+ from a cuase with a certain degree of assurance
+
+9. What are the different variable reduction techniques you know of?
+Information Value Criteria
+Clustering
+Principal components
+Factor analysis
+
+10. WHat is the difference between parametric models and non parametric models?
+The data is assumed to follow some theoretical distribution in parametric model - normal or binomial distribution
+Non Parametric models does not assume any theoretical distribution of the data - smoothing models
+
+11. Correclation vs regression
+correclation - two way phenomenon, where regression is one way
+
+
